6400ae7be9c29dfd07ea4d7d4b24cc977c1af645
Yuhi Ishikura <ishikura@mk.ces.kyutech.ac.jp>
2012-06-17 13:30:39 +0900

    Adds JButtonExample as an observer pattern.

---
+++ src/jp/uphy/dsptn/observer/JButtonExample.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2012 uphy.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.uphy.dsptn.observer;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+
+
+/**
+ * @author Yuhi Ishikura
+ */
+public class JButtonExample {
+
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    JButton button = new JButton("Button");
+    button.addActionListener(new MyButtonListener());
+    frame.add(button);
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  static class MyButtonListener implements ActionListener {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      JOptionPane.showMessageDialog(null, "Button pressed");
+    }
+
+  }
+
+}
