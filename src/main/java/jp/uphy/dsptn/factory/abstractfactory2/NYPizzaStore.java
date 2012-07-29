44058e41a7061ac750414810d21106c7be35504f
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2012-07-30 06:35:05 +0900

    Adding factory example by guice.

---
+++ src/main/java/jp/uphy/dsptn/factory/abstractfactory2/NYPizzaStore.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2012 uphy.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.uphy.dsptn.factory.abstractfactory2;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+
+/**
+ * @author yuhi
+ */
+public class NYPizzaStore extends PizzaStore {
+
+  private Injector injector;
+
+  /**
+   * {@link NYPizzaStore}オブジェクトを構築します。
+   */
+  public NYPizzaStore() {
+    this.injector = Guice.createInjector(new NYStylePizzaIngredientModule());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected Pizza createPizza(String type) {
+    Pizza pizza = null;
+
+    if (type.equals("cheese")) {
+      pizza = this.injector.getInstance(CheesePizza.class);
+    } else if (type.equals("pepperoni")) {
+      pizza = this.injector.getInstance(PepperoniPizza.class);
+    } else if (type.equals("veggie")) {
+      pizza = this.injector.getInstance(VeggiePizza.class);
+    } else if (type.equals("clam")) {
+      pizza = this.injector.getInstance(ClamPizza.class);
+    }
+    return pizza;
+  }
+
+}
---
+++ src/main/java/jp/uphy/dsptn/factory/abstractfactory2/Pizza.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2012 uphy.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.uphy.dsptn.factory.abstractfactory2;
+
+/**
+ * @author yuhi
+ */
+public abstract class Pizza {
+
+  protected String name;
+  protected Dough dough;
+  protected Sauce sauce;
+  protected Veggies veggies[];
+  protected Cheese cheese;
+  protected Pepperoni pepperoni;
+  protected Clams clam;
+
+  void bake() {
+
+  }
+
+  void cut() {
+
+  }
+
+  void box() {
+
+  }
+
+  void setName(String name) {
+    this.name = name;
+  }
+
+  String getName() {
+    return this.name;
+  }
+
+}
