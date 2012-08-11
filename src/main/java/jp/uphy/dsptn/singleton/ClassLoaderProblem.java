1c074ed075d96d6065cbbc83bc8e47b47d102489
Yuhi Ishikura <ishikura@mk.ces.kyutech.ac.jp>
2012-08-11 17:14:44 +0900

    Adds ClassLoaderProblem example for the Singleton Pattern.

---
+++ src/main/java/jp/uphy/dsptn/singleton/ClassLoaderProblem.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2012 uphy.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.uphy.dsptn.singleton;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+/**
+ * 複数のクラスローダでシングルトンのクラスを読み込んだ時にインスタンスが二回生成されてしまう現象の確認。
+ * 
+ * @author Yuhi Ishikura
+ */
+public class ClassLoaderProblem {
+
+  static URL[] SAMPLE_JAR_URL;
+
+  static {
+    try {
+      SAMPLE_JAR_URL = new URL[] {new File("singleton-sample/singleton-sample.jar").toURI().toURL()};
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void main(String[] args) {
+    Class<?> c1 = loadWithNewClassLoader();
+    Class<?> c2 = loadWithNewClassLoader();
+
+    callGetInstance(c1);
+    callGetInstance(c2);
+  }
+
+  /**
+   * singleton-sample.jarに含まれるMySingletonクラスを新しいクラスローダで読み込み返します。
+   * 
+   * @return 新しいクラスローダで読み込んだsingleton-sample.jarに含まれるMySingletonクラス
+   */
+  private static Class<?> loadWithNewClassLoader() {
+    try (URLClassLoader classLoader = new URLClassLoader(SAMPLE_JAR_URL)) {
+      return classLoader.loadClass("jp.uphy.dsptn.singleton.MySingleton");
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e1) {
+      throw new RuntimeException(e1);
+    }
+  }
+
+  /**
+   * 与えられたクラスのstaticメソッドgetInstance()を呼び出します。
+   * 
+   * @param c クラスオブジェクト
+   */
+  private static void callGetInstance(Class<?> c) {
+    try {
+      c.getMethod("getInstance").invoke(null);
+    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
